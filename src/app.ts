f79ad7afb619c4a84f5f06a57fb77688bde4a6f4
Sajmul <sajmul1427@gmail.com>
2025-06-21 21:42:21 +0600

    error handling middleware and 404 route not found middleware done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Roadmap server is running!");
+});
 
+app.use((req: Request, res: Response) => {
+  res.json({ message: "Route not found", success: false });
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    if(error) {
+        res.status(400).json({
+            message: error.message === 'ValidationError' ? 'Validation failed' : error.message === 'CastError' ? "Cannot get by this id" : 'Unknown Error Occured',
+            success: false,
+            error
+        })
+    }
+})
 
 export default app;
