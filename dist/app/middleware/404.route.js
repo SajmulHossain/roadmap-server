b9f0bdb66af5bd7f1dc9da0d927f2e6eb973c415
Sajmul <sajmul1427@gmail.com>
2025-08-01 00:04:37 +0600

    logout api solved

--- dist/app/middleware/404.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routeNotFound = void 0;
-const routeNotFound = (req, res) => {
-    res.status(404).json({ message: "Route not found", success: false, route: req.url });
-};
-exports.routeNotFound = routeNotFound;
--- src/app/auth/auth.route.ts
+++ src/app/auth/auth.route.ts
@@ -54,7 +54,7 @@ authRouter.post("/login", async (req: Request, res: Response) => {
     });
 });
 
-authRouter.get("/logout", (req: Request, res: Response) => {
+authRouter.post("/logout", (req: Request, res: Response) => {
   res
     .clearCookie("token", {
       maxAge: 0,
